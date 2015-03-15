1aaed78e6249bcfe7e3750c99af8ed66f4220394
Kevin McCarthy <me@kevinmccarthy.org>
2015-03-14 17:41:25 -1000

    fix tests

--- tests/integration/test_finder.py
+++ tests/integration/test_finder.py
@@ -1,15 +1,14 @@
-from ..util import configure_settings
-configure_settings()
+from django.conf import settings
 
-from django.test.utils import override_settings
+settings.configure(DEBUG=True)
+
 from npm.finders import NpmFinder
 
-def test_finder(tmpdir):
-    pjson = tmpdir.join('package.json') \
-    .write('''{
+def test_get_files(tmpdir):
+    pjson = tmpdir.join('package.json')
+    pjson.write('''{
     "name": "test",
     "dependencies": {"mocha": "*"}
     }''')
-    with override_settings(NPM_PREFIX_PATH=str(tmpdir)):
-        f = NpmFinder()
-        print list(f.list([]))
+    get_files(str(pjson), None)
+
