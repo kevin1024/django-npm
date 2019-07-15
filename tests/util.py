22c5372bc051795386363b52bd05697e2a85cfc9
alexsilva <geniofuturo@gmail.com>
2019-07-15 10:54:22 -0300

    Update test; Add npm app.

--- tests/util.py
+++ tests/util.py
@@ -1,12 +1,15 @@
 from django.conf import settings
+import django
 
 
 def configure_settings():
     settings.configure(
         DEBUG=True,
+        INSTALLED_APPS=['npm'],
         CACHES={
             'default': {
                 'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
             }
         }
     )
+    django.setup()
