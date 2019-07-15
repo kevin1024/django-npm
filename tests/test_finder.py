7b855aa121cba7310b77a742a2548881a84ece5e
alexsilva <geniofuturo@gmail.com>
2019-07-15 10:33:19 -0300

    pep8

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -1,4 +1,5 @@
 from .util import configure_settings
+
 configure_settings()
 
 import pytest
@@ -28,34 +29,41 @@ def test_get_files(npm_dir):
     files = get_files(storage, match_patterns='*')
     assert any([True for _ in files])
 
+
 def test_finder_list_all(npm_dir):
     f = NpmFinder()
     assert any([True for _ in f.list()])
+
 
 def test_finder_find(npm_dir):
     f = NpmFinder()
     file = f.find('mocha/mocha.js')
     assert file
 
+
 def test_finder_in_subdirectory(npm_dir):
     with override_settings(NPM_STATIC_FILES_PREFIX='lib'):
         f = NpmFinder()
         assert f.find('lib/mocha/mocha.js')
+
 
 def test_finder_with_patterns_in_subdirectory(npm_dir):
     with override_settings(NPM_STATIC_FILES_PREFIX='lib', NPM_FILE_PATTERNS={'mocha': ['*']}):
         f = NpmFinder()
         assert f.find('lib/mocha/mocha.js')
 
+
 def test_finder_with_patterns_in_directory_component(npm_dir):
     with override_settings(NPM_STATIC_FILES_PREFIX='lib', NPM_FILE_PATTERNS={'mocha': ['*/*js']}):
         f = NpmFinder()
         assert f.find('lib/mocha/lib/test.js')
 
+
 def test_no_matching_paths_returns_empty_list(npm_dir):
     with override_settings(NPM_FILE_PATTERNS={'foo': ['bar']}):
         f = NpmFinder()
         assert f.find('mocha/mocha.js') == []
+
 
 def test_finder_cache(npm_dir):
     with override_settings(NPM_FINDER_USE_CACHE=True):
@@ -63,6 +71,7 @@ def test_finder_cache(npm_dir):
         f.list()
         assert f.cached_list is not None
         assert f.list() is f.cached_list
+
 
 def test_finder_no_cache(npm_dir):
     with override_settings(NPM_FINDER_USE_CACHE=False):
