9e1cc50fa525a8ced1d4ed4e7d1dc685b6e71ebd
David Nugent <davidn@uniquode.io>
2021-09-10 22:31:01 +1000

    Simplify get_files, using pathlib & glob
    - do npm_install once as a session fixture rather than for each test
    - use correct target fortest_get_files*() tests
    - fix and remove limit on search depth, follow unix standard for '**' handling
    - support use of yarn

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
 from .util import configure_settings
 configure_settings()
 
@@ -6,56 +10,116 @@ import pytest
 from django.core.files.storage import FileSystemStorage
 from django.test.utils import override_settings
 
-from npm.finders import get_files
-from npm.finders import NpmFinder
-from npm.finders import npm_install
+from npm.finders import get_files, NpmFinder, npm_install
 
 
-@pytest.yield_fixture
-def npm_dir(tmpdir):
-    package_json = tmpdir.join('package.json')
-    package_json.write('''{
+@pytest.fixture(scope='session')
+def setup_npm_dir():
+    # don't use py.tmpdir as it is scope='function'
+    with TemporaryDirectory() as tmppath:
+        tmpdir = Path(tmppath).resolve(strict=True)
+        package_json = tmpdir / 'package.json'
+        package_json.write_text('''{
     "name": "test",
     "dependencies": {"mocha": "*"}
-    }''')
+}''')
+        with override_settings(NPM_ROOT_PATH=str(tmpdir)):
+            npm_install()
+            yield tmpdir
+
+
+@pytest.fixture(scope='function')
+def npm_dir(setup_npm_dir):
+    tmpdir = setup_npm_dir
     with override_settings(NPM_ROOT_PATH=str(tmpdir)):
-        npm_install()
         yield tmpdir
 
 
-def test_get_files(npm_dir):
-    storage = FileSystemStorage(location=str(npm_dir))
-    files = get_files(storage, match_patterns='*')
-    assert any([True for _ in files])
+@pytest.fixture(scope='function')
+def storage(npm_dir):
+    return FileSystemStorage(location=str(npm_dir / 'node_modules'))
+
+
+def test_get_files_all(storage):
+    files = list(get_files(storage, match_patterns='*'))
+    assert len(files)
+    assert all(filename for filename in files)
+
+
+def test_get_files_with_patterns(storage):
+    files_all = list(get_files(storage, match_patterns=['**/*.js', '**/*.css']))
+    assert len(files_all)
+    assert all(path for path in files_all)
+    assert all(path.suffix in ('.js', '.css') for path in files_all)
+
+    files = list(get_files(storage, match_patterns=['*/*.js', '*/*.css']))
+    assert len(files)
+    assert all(path for path in files)
+    assert all(path.suffix in ('.js', '.css') for path in files_all)
+
+    assert files != files_all
+
 
 def test_finder_list_all(npm_dir):
     f = NpmFinder()
-    assert any([True for _ in f.list()])
+    results = list(f.list())
+    assert len(results)
+    assert all(isinstance(result, tuple) for result in results)
+    assert all(path for path, storage in results)
+    assert all(storage is not None for filename, storage in results)
+
+
+def test_finder_list_in_module(npm_dir):
+    with override_settings(NPM_FILE_PATTERNS={'mocha': ['*']}):
+        f = NpmFinder()
+        results = list(f.list())
+        assert len(results)
+        assert all(isinstance(result, tuple) for result in results)
+        assert all(path for path, storage in results)
+        assert all(storage is not None for filename, storage in results)
+
+
+def test_finder_list_files_in_module(npm_dir):
+    with override_settings(NPM_FILE_PATTERNS={'mocha': ['*.js','*.css',]}):
+        f = NpmFinder()
+        results = list(f.list())
+        assert len(results)
+        assert all(isinstance(result, tuple) for result in results)
+        assert all(path for path, storage in results)
+        assert all(storage is not None for filename, storage in results)
+
 
 def test_finder_find(npm_dir):
     f = NpmFinder()
     file = f.find('mocha/mocha.js')
     assert file
 
+
 def test_finder_in_subdirectory(npm_dir):
     with override_settings(NPM_STATIC_FILES_PREFIX='lib'):
         f = NpmFinder()
         assert f.find('lib/mocha/mocha.js')
 
+
 def test_finder_with_patterns_in_subdirectory(npm_dir):
-    with override_settings(NPM_STATIC_FILES_PREFIX='lib', NPM_FILE_PATTERNS={'mocha': ['*']}):
+    with override_settings(NPM_STATIC_FILES_PREFIX='lib',
+                           NPM_FILE_PATTERNS={'mocha': ['*']}):
         f = NpmFinder()
         assert f.find('lib/mocha/mocha.js')
 
+
 def test_finder_with_patterns_in_directory_component(npm_dir):
-    with override_settings(NPM_STATIC_FILES_PREFIX='lib', NPM_FILE_PATTERNS={'mocha': ['*/*js']}):
+    with override_settings(NPM_STATIC_FILES_PREFIX='lib',
+                           NPM_FILE_PATTERNS={'mocha': ['*/*js']}):
         f = NpmFinder()
         assert f.find('lib/mocha/lib/test.js')
+
 
 def test_no_matching_paths_returns_empty_list(npm_dir):
     with override_settings(NPM_FILE_PATTERNS={'foo': ['bar']}):
         f = NpmFinder()
         assert f.find('mocha/mocha.js') == []
+
 
 def test_finder_cache(npm_dir):
     with override_settings(NPM_FINDER_USE_CACHE=True):
@@ -63,6 +127,7 @@ def test_finder_cache(npm_dir):
         f.list()
         assert f.cached_list is not None
         assert f.list() is f.cached_list
+
 
 def test_finder_no_cache(npm_dir):
     with override_settings(NPM_FINDER_USE_CACHE=False):
